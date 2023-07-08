28b4fadc8443b63627265206c4ca435181b1aa3f
LCGeon <corjs8449@naver.com>
2023-07-09 05:13:22 +0900

    Feat: 대표적인 Primmary Input 컴포넌트 추가

---
+++ src/components/ui/PrimaryInput.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+import { GRAY } from '../../constants/color';
+
+interface Props {
+  placeholder: string;
+}
+
+function PrimaryInput({ placeholder }: Props) {
+  return <Input type='text' placeholder={placeholder} />;
+}
+
+const Input = styled.input`
+  width: 100%;
+  height: 40px;
+  border: 1px solid ${GRAY};
+  border-radius: 8px;
+
+  &::placeholder {
+    color: ${GRAY};
+    font-size: 14px;
+    padding: 12px 16px;
+  }
+`;
+export default PrimaryInput;
