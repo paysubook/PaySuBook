ceb5832321047ef31e701a01d8b8493f5a4092b1
LCGeon <corjs8449@naver.com>
2023-07-09 05:09:36 +0900

    Feat: AppInner에 라우터 초기 설정 및 Main, SignUp 라우트 추가

--- src/AppInner.tsx
+++ src/AppInner.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
+const Main = React.lazy(() => import('./pages/Main'));
+const SignUp = React.lazy(() => import('./pages/SignUp'));
 function AppInner() {
-  return <div>AppInner</div>;
+  return (
+    <div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/signup' element={<SignUp />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
 }
 
 export default AppInner;
