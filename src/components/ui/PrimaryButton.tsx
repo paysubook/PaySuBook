d71d444eb92f7a811d241abe07a154a0c2621936
LCGeon <corjs8449@naver.com>
2023-07-09 05:12:58 +0900

    Feat: 대표적인 Primary Button 컴포넌트 추가

---
+++ src/components/ui/PrimaryButton.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+import { PRIMARY } from '../../constants/color';
+
+interface Props {
+  children: string;
+  color: string;
+  background: string;
+}
+function PrimaryButton({ children, color, background }: Props) {
+  return (
+    <S.Button color={color} background={background}>
+      {children}
+    </S.Button>
+  );
+}
+
+const S = {
+  Button: styled.button<Pick<Props, 'color'> & { background?: string }>`
+    width: 100%;
+    height: 40px;
+    font-size: 16px;
+    font-weight: bold;
+    background-color: ${(props) => props.background};
+    border: 1px solid ${PRIMARY};
+    border-radius: 15px;
+    color: ${(props) => props.color};
+  `,
+};
+export default PrimaryButton;
