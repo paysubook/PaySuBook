2beb2fa2965f239298535df9890e679bd9c778db
LCGeon <corjs8449@naver.com>
2023-07-11 22:35:28 +0900

    Feat: TabBar의 코드 단축 및 홈 부분에 CostSummary, 내역 부분에 AssetStatus, FinanceSummary, Chips 컴포넌트 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,74 +1,122 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { LIGHT_GRAY } from '../constants/color';
-import { MdHome, MdFormatListBulleted } from 'react-icons/md';
+import { GRAY, LIGHT_GRAY, PRIMARY } from '../constants/color';
 import TabBar from '../components/ui/TabBar';
-import CompareOutlay from '../components/CompareOutlay';
+import CompareOutlay from '../components/home/CompareOutlay';
+import HomeIcon from '../constants/img/HomeIcon.png';
+import {
+  MdHome,
+  MdFormatListBulleted,
+  MdAttachMoney,
+  MdCreditCard,
+  MdAdd,
+} from 'react-icons/md';
+import CostSummary from '../components/home/CostSummary';
+import AssetStatus from '../components/home/AssetStatus';
+import FinanceSummary from '../components/home/FinanceSummary';
+import Chips from '../components/ui/Chips';
 
 function Home() {
   const [selectedTabId, setSelectedTabId] = useState(1);
-  const [isTap, setIsTap] = useState(true);
+  const [seletedChip, setSelectedChip] = useState(1);
 
   const handleTabPress = (tabId) => {
     if (selectedTabId !== tabId) {
       setSelectedTabId(tabId);
-      setIsTap(!isTap);
     }
   };
 
-  if (isTap) {
-    return (
-      <S.HomeLayOut>
+  const handleChipPress = (chipId) => {
+    if (seletedChip !== chipId) {
+      setSelectedChip(chipId);
+    }
+  };
+
+  const getNowMonth = () => {
+    return new Date().getMonth() + 1;
+  };
+
+  return (
+    <S.HomeLayOut>
+      {selectedTabId === 1 ? (
         <S.HomeItemContainer>
           <S.CompareOutlay>
             <CompareOutlay date={7} price={84000} />
           </S.CompareOutlay>
+          <S.CostSummary>
+            <CostSummary icon={<MdAttachMoney />} price={85000} />
+          </S.CostSummary>
+          <S.CostSummary>
+            <CostSummary icon={<MdCreditCard />} price={33350120} />
+          </S.CostSummary>
+          <S.HomeNotice>
+            <div>
+              <S.HomeIcon src={HomeIcon} alt='HomeIcon' />
+            </div>
+            <S.HomePhrase>
+              나는 어디에 돈을 많이 썼는지 확인해볼까요?
+            </S.HomePhrase>
+          </S.HomeNotice>
         </S.HomeItemContainer>
-        <S.HomeTapBar>
-          <div style={{ width: '100%' }} onClick={() => handleTabPress(1)}>
-            <TabBar selected={selectedTabId === 1}>
-              <S.TabItem>
-                <MdHome />
-                <S.ItemName>홈</S.ItemName>
-              </S.TabItem>
-            </TabBar>
-          </div>
-          <div style={{ width: '100%' }} onClick={() => handleTabPress(2)}>
-            <TabBar selected={selectedTabId === 2}>
-              <S.TabItem>
-                <MdFormatListBulleted />
-                <S.ItemName>내역</S.ItemName>
-              </S.TabItem>
-            </TabBar>
-          </div>
-        </S.HomeTapBar>
-      </S.HomeLayOut>
-    );
-  } else {
-    return (
-      <S.HomeLayOut>
-        <div>내역입니다 ^^ ^^</div>
-        <S.HomeTapBar>
-          <div style={{ width: '100%' }} onClick={() => handleTabPress(1)}>
-            <TabBar selected={selectedTabId === 1}>
-              <S.TabItem>
-                <MdHome />
-                <S.ItemName>홈</S.ItemName>
-              </S.TabItem>
-            </TabBar>
-          </div>
-          <div style={{ width: '100%' }} onClick={() => handleTabPress(2)}>
-            <TabBar selected={selectedTabId === 2}>
-              <S.TabItem>
-                <MdFormatListBulleted />
-                <S.ItemName>내역</S.ItemName>
-              </S.TabItem>
-            </TabBar>
-          </div>
-        </S.HomeTapBar>
-      </S.HomeLayOut>
-    );
-  }
+      ) : (
+        <S.HomeHistoryLayout>
+          <S.HomeHistory>
+            <S.AssetStatus>
+              <div>
+                <AssetStatus month={getNowMonth()} asset={5055000} />
+              </div>
+              <div>
+                <S.ViewAllBtn>전체보기</S.ViewAllBtn>
+              </div>
+            </S.AssetStatus>
+            <S.FinanceSummary>
+              <FinanceSummary profit={213123} expenditure={4141312} />
+            </S.FinanceSummary>
+            <S.HistoryChips>
+              <Chips
+                width='50px'
+                selected={seletedChip === 1}
+                onClick={() => handleChipPress(1)}
+              >
+                전체
+              </Chips>
+              <Chips
+                width='50px'
+                selected={seletedChip === 2}
+                onClick={() => handleChipPress(2)}
+              >
+                수입
+              </Chips>
+              <Chips
+                width='50px'
+                selected={seletedChip === 3}
+                onClick={() => handleChipPress(3)}
+              >
+                지출
+              </Chips>
+            </S.HistoryChips>
+          </S.HomeHistory>
+          <S.AddHistoryBtn>
+            <MdAdd />
+          </S.AddHistoryBtn>
+        </S.HomeHistoryLayout>
+      )}
+      <S.HomeTapBar>
+        <TabBar
+          selected={selectedTabId === 1}
+          onClick={() => handleTabPress(1)}
+          icon={<MdHome />}
+          label='홈'
+        />
+        <TabBar
+          selected={selectedTabId === 2}
+          onClick={() => handleTabPress(2)}
+          icon={<MdFormatListBulleted />}
+          label='내역'
+        />
+      </S.HomeTapBar>
+    </S.HomeLayOut>
+  );
 }
 
 const S = {
@@ -83,20 +131,9 @@ const S = {
     bottom: 0px;
     justify-content: space-around;
   `,
-  TabItem: styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    display: flex;
-    font-size: 22px;
-  `,
-  ItemName: styled.span`
-    margin-top: 5px;
-    font-size: 14px;
-  `,
   HomeItemContainer: styled.div`
     display: flex;
+    width: 100%;
     align-items: center;
     justify-content: center;
     flex-direction: column;
@@ -105,7 +142,79 @@ const S = {
     width: 90%;
     background-color: white;
     border-radius: 17px;
-    margin-top: 32px;
+    margin: 32px 0px 20px 0px;
+  `,
+  CostSummary: styled.div`
+    width: 90%;
+    margin: 5px 0px;
+  `,
+  HomeNotice: styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: white;
+    border-radius: 16px;
+    padding: 20px;
+    margin-top: 20px;
+    width: 79%;
+  `,
+  HomeIcon: styled.img`
+    width: 130px;
+    height: 130px;
+  `,
+  HomePhrase: styled.div`
+    font-size: 13px;
+    font-weight: bold;
+    margin-top: 25px;
+  `,
+  HomeHistoryLayout: styled.div`
+    display: flex;
+    position: relative;
+    height: 90vh;
+  `,
+  AddHistoryBtn: styled.button`
+    display: flex;
+    align-items: center;
+    position: absolute;
+    justify-content: center;
+    bottom: 10px;
+    right: 20px;
+    width: 48px;
+    height: 48px;
+    font-size: 24px;
+    border: none;
+    background-color: ${PRIMARY};
+    color: white;
+    border-radius: 50%;
+  `,
+  HomeHistory: styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: white;
+  `,
+  AssetStatus: styled.div`
+    display: flex;
+    width: 90%;
+    justify-content: space-between;
+    margin: 20px 0px;
+  `,
+  ViewAllBtn: styled.button`
+    border: none;
+    padding: 10px;
+    font-size: 11px;
+    background-color: ${GRAY};
+    font-weight: bold;
+  `,
+  FinanceSummary: styled.div`
+    width: 90%;
+  `,
+  HistoryChips: styled.div`
+    display: flex;
+    width: 90%;
+    margin-top: 15px;
   `,
 };
 
