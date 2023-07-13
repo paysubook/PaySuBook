8e232f74d8301558581fe3ab676166352e6a4409
LCGeon <corjs8449@naver.com>
2023-07-13 21:14:29 +0900

    Style: selected Props의 네이밍을 알아보기쉽게 isActive로 변경 및 불필요한 width Props 삭제

--- src/components/ui/Chips.tsx
+++ src/components/ui/Chips.tsx
@@ -2,32 +2,31 @@ import React from 'react';
 import styled from 'styled-components';
 import { LIGHT_GRAY, PRIMARY } from '../../constants/color';
 interface Props {
-  selected: boolean;
-  width: string;
+  isActive: boolean;
   label: string;
   onClick?: () => void;
 }
-function Chip({ selected, width, onClick, label }: Props) {
+function Chip({ isActive, onClick, label }: Props) {
   return (
-    <S.BasicChip onClick={onClick} width={width} selected={selected}>
+    <S.BasicChip onClick={onClick} isActive={isActive}>
       {label}
     </S.BasicChip>
   );
 }
 
 const S = {
-  BasicChip: styled.div<{ selected: boolean; width: string }>`
+  BasicChip: styled.div<{ isActive: boolean }>`
     display: flex;
     justify-content: center;
     align-items: center;
-    width: ${(props) => props.width};
+    width: 50px;
     height: 27px;
     font-size: 10px;
     font-weight: bold;
     border-radius: 20px;
     text-align: center;
-    background-color: ${(props) => (props.selected ? PRIMARY : LIGHT_GRAY)};
-    color: ${(props) => (props.selected ? 'white' : '')};
+    background-color: ${(props) => (props.isActive ? PRIMARY : LIGHT_GRAY)};
+    color: ${(props) => (props.isActive ? 'white' : '')};
   `,
 };
 export default Chip;
