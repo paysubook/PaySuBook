cdc5b302e9b24af0676abc784fff4b356ee21edd
LCGeon <corjs8449@naver.com>
2023-07-26 22:52:38 +0900

    Feat: PrimaryButton 컴포넌트에 disabled props 추가 및 css 변경

--- src/components/ui/PrimaryButton.tsx
+++ src/components/ui/PrimaryButton.tsx
@@ -7,22 +7,35 @@ interface Props {
   color: string;
   background: string;
   onClick?: () => void;
+  disabled?: boolean;
 }
-function PrimaryButton({ children, color, background, onClick }: Props) {
+function PrimaryButton({
+  children,
+  color,
+  background,
+  onClick,
+  disabled,
+}: Props) {
   return (
-    <S.Button color={color} background={background} onClick={onClick}>
+    <S.Button
+      color={color}
+      background={background}
+      onClick={onClick}
+      disabled={disabled}
+    >
       {children}
     </S.Button>
   );
 }
 
 const S = {
-  Button: styled.button<Pick<Props, 'color'> & { background?: string }>`
+  Button: styled.button<Props>`
     width: 100%;
     height: 40px;
     font-size: 16px;
     font-weight: bold;
-    background-color: ${(props) => props.background};
+    background-color: ${(props) =>
+      props.disabled ? 'gray' : props.background};
     border: 1px solid ${PRIMARY};
     border-radius: 15px;
     color: ${(props) => props.color};
